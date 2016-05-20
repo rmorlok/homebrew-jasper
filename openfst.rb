3f6aba543e8d0013b3a8b4151382dbb85d5069c5
Ryan Morlok <ryan.morlok@morlok.com>
2016-05-19 23:12:08 -0500

    initial version of the modified version of the openfst formula

---
+++ openfst.rb
@@ -0,0 +1,47 @@
+class Openfst < Formula
+  homepage "http://www.openfst.org/"
+  url "http://openfst.org/twiki/pub/FST/FstDownload/openfst-1.5.1.tar.gz"
+  sha256 "bb0c0e6cf8bed843c36f412f66abfd87604b3220773e4c25fd723a7a5759a3f2"
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--enable-compact-fsts",
+                          "--enable-compress",
+                          "--enable-const-fsts",
+                          "--enable-far",
+                          "--enable-linear-fsts",
+                          "--enable-lookahead-fsts",
+                          "--enable-mpdt",
+                          "--enable-ngram-fsts",
+                          "--enable-pdt",
+                          "--enable-static",
+                          "--enable-shared"
+    system "make", "install"
+  end
+
+  test do
+    # Based on http://openfst.org/twiki/bin/view/FST/FstExamples.
+    # Makes symbol table.
+    File.open("ascii.syms", "w") do |sink|
+      sink.puts "<epsilon>\t0"
+      sink.puts "<space>\t32"
+      33.upto(126) { |i| sink.puts "#{i.chr}\t#{i}" }
+    end
+    # Makes arclist for downcasing FST.
+    File.open("downcase.arc", "w") do |sink|
+      sink.puts "0\t0\t<epsilon>\t<epsilon>"
+      sink.puts "0\t0\t<space>\t<space>"
+      33.upto(126) { |i| sink.puts "0\t0\t#{i.chr}\t#{i.chr.downcase}" }
+      sink.puts "0"
+    end
+    ## Compile the machine
+    system bin/"fstcompile", "--isymbols=ascii.syms",
+                             "--osymbols=ascii.syms",
+                             "downcase.arc",
+                             "downcase.fst"
+  end
+end
