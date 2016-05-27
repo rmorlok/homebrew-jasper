e598fd390e1997f2b2f60808478cff4d8b316ede
Ryan Morlok <ryan.morlok@morlok.com>
2016-05-26 21:56:07 -0500

    testing changes

--- phonetisaurus.rb
+++ phonetisaurus.rb
@@ -3,11 +3,13 @@ class Phonetisaurus < Formula
   url "https://github.com/rmorlok/Phonetisaurus/archive/v0.1.tar.gz"
   sha256 "49975ed9eff8f8f325d6f599c73ef335ab8fd44aedc653e4b1ba06cdd0c646bd"
 
+  depends_on "rmorlok/jasper/openfst"
+
   needs :cxx11
 
   def install
     ENV.cxx11
-    system 'cd src'
+    system 'cd', 'src'
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
