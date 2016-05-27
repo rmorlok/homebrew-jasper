e46b450b2dab714599e8ed64ba0cabc63a3f81ff
Ryan Morlok <ryan.morlok@morlok.com>
2016-05-26 22:07:56 -0500

    more fixes

--- phonetisaurus.rb
+++ phonetisaurus.rb
@@ -1,5 +1,5 @@
 class Phonetisaurus < Formula
-  homepage "http://www.openfst.org/"
+  homepage "https://github.com/AdolfVonKleist/Phonetisaurus"
   url "https://github.com/rmorlok/Phonetisaurus/archive/v0.1.tar.gz"
   sha256 "49975ed9eff8f8f325d6f599c73ef335ab8fd44aedc653e4b1ba06cdd0c646bd"
 
@@ -9,7 +9,7 @@ class Phonetisaurus < Formula
 
   def install
     ENV.cxx11
-    system 'cd', 'src'
+    Dir.chdir "src"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
