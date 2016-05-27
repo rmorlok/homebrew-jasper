6738a563fb9ef2773ec235f14e34c1b51cdb9f57
Ryan Morlok <ryan.morlok@morlok.com>
2016-05-26 21:40:55 -0500

    name fix

--- phonetisaurus.rb
+++ phonetisaurus.rb
@@ -1,4 +1,4 @@
-class Openfst < Formula
+class Phonetisaurus < Formula
   homepage "http://www.openfst.org/"
   url "https://github.com/rmorlok/Phonetisaurus/archive/v0.1.tar.gz"
   sha256 "6593edb401d047d942365437be012d974990609b6eb89814d1c6422a4161771e"
